0655302b96b8d63671f0e3630c2278c11c2a7593
Atsushi Eno <atsushieno@gmail.com>
2025-02-28 12:45:18 +0900

    bump all deps, Gradle, and AGP.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.serializationPlugin) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
--- cipackageinstaller/build.gradle.kts
+++ cipackageinstaller/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.serializationPlugin)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
